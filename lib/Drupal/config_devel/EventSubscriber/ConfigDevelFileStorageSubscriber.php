546ec062c33a100f16aba80b6db5325456619e8b
beejeebus <justin.p.randell@gmail.com>
2014-04-16 00:38:48 +1000

    Initial commit.

---
+++ lib/Drupal/config_devel/EventSubscriber/ConfigDevelFileStorageSubscriber.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\config_devel\EventSubscriber\ConfigDevelFileStorageSubscriber.
+ */
+
+namespace Drupal\config_devel\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Config\FileStorage;
+use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Config\ConfigRenameEvent;
+use Drupal\Core\Config\ConfigImporterEvent;
+use Drupal\Core\Config\ConfigEvents;
+
+/**
+ * ConfigDevelFileStorageSubscriber subscriber for configuration CRUD events.
+ */
+class ConfigDevelFileStorageSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The configuration file storage object.
+   *
+   * @var \Drupal\Core\Config\FileStorage
+   */
+  protected $filestorage;
+
+  /**
+   * Constructs the ConfigDevelFileStorageSubscriber object.
+   *
+   * @param \Drupal\Core\Config\FileStorage $filestorage
+   */
+  function __construct(FileStorage $filestorage) {
+    $this->filestorage = $filestorage;
+  }
+
+  /**
+   * React to configuration ConfigEvent::SAVE events.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   *   The event to process.
+   */
+  public function onConfigSave(ConfigCrudEvent $event) {
+    $config = $event->getConfig();
+    $this->filestorage->write($config->getName(), $config->get());
+  }
+
+  /**
+   * React to configuration ConfigEvent::RENAME events.
+   *
+   * @param \Drupal\Core\Config\ConfigRenameEvent $event
+   *   The event to process.
+   */
+  public function onConfigRename(ConfigRenameEvent $event) {
+    $config = $event->getConfig();
+    $this->filestorage->write($config->getName(), $config->get());
+    $this->filestorage->delete($config->getOldName());
+  }
+
+  /**
+   * React to configuration ConfigEvent::DELETE events.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   *   The event to process.
+   */
+  public function onConfigDelete(ConfigCrudEvent $event) {
+    $config = $event->getConfig();
+    $this->filestorage->delete($config->getOldName());
+  }
+
+  /**
+   * React to configuration ConfigEvent::IMPORT events.
+   *
+   * @param \Drupal\Core\Config\ConfigImporterEvent $event
+   *   The event to process.
+   */
+  public function onConfigImport(ConfigImporterEvent $event) {
+  }
+
+  /**
+   * Registers the methods in this class that should be listeners.
+   *
+   * @return array
+   *   An array of event listener definitions.
+   */
+  static function getSubscribedEvents() {
+    $events[ConfigEvents::SAVE][] = array('onConfigSave', 10);
+    $events[ConfigEvents::DELETE][] = array('onConfigDelete', 10);
+    $events[ConfigEvents::RENAME][] = array('onConfigRename', 10);
+    $events[ConfigEvents::IMPORT][] = array('onConfigImport', 10);
+    return $events;
+  }
+
+}
